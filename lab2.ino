bf238e7cec7ca78db8c1a2286f383e6fc804857a
cyuun <41702930+cyuun@users.noreply.github.com>
2019-02-04 21:05:54 -0700

    Update lab2.ino

--- lab2.ino
+++ lab2.ino
@@ -1,6 +1,4 @@
-// setup routine to initialize the process
-  
-// variabling the pins
+// global variabling the pins
 int button1 = 2;
 int button2 = 3;
 int switch1 = 2;
@@ -11,6 +9,7 @@ int led4 = 12;
 int led5 = 13;
 int button1state, button2state, switch1state;
 
+// setup routine to initialize the process  
 void setup() {
   // initializing the pins
   pinMode(button1, INPUT);
@@ -29,13 +28,74 @@ void setup() {
 // loop routine main body
 void loop() {
   
+  // acquiring those values
   button1state = digitalRead(button1);
   button2state = digitalRead(button2);
   switch1state = digitalRead(switch1);
 
-  Serial.println(button1state);
-  Serial.println(button2state);
-  Serial.println(switch1state);  
+  // Serial.println(button1state);
+  // Serial.println(button2state);
+  // Serial.println(switch1state);
   
-  
+  // first the switch is a modifier, meaning 2 options
+  if (switch1state) {
+    // then you have 3 options for each switch state
+    if (button1state && !button2state) {
+      // option 1 first button is pressed and second isnt
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, HIGH);
+      digitalWrite(led4, LOW);
+      digitalWrite(led5, LOW);
+    } else if (!button1state && button2state) {
+      // option 2 first button not pressed and second is
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, LOW);
+      digitalWrite(led4, LOW);
+      digitalWrite(led5, HIGH);
+    } else if (button1state && button2state) {
+      // option 3 both buttons pressed
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, HIGH);
+      digitalWrite(led4, HIGH);
+      digitalWrite(led5, LOW);
+    } else {
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
+      digitalWrite(led4, LOW);
+      digitalWrite(led5, LOW);
+    }
+  } else {
+    // switch is off but you still have the same 3 options just different results
+    if (button1state && !button2state) {
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, LOW);
+      digitalWrite(led4, LOW);
+      digitalWrite(led5, LOW);
+    } else if (!button1state && button2state) {
+      // I might as well take this moment to explain why each statement contains all 5 write statements every time
+      // it allows a "seamless" transition when moving from pressing 1 button to pressing both
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, HIGH);
+      digitalWrite(led4, HIGH);
+      digitalWrite(led5, HIGH);
+    } else if (button1state && button2state) {
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, HIGH);
+      digitalWrite(led4, HIGH);
+      digitalWrite(led5, HIGH);
+    } else {
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
+      digitalWrite(led4, LOW);
+      digitalWrite(led5, LOW);
+    }
+  }
 }
