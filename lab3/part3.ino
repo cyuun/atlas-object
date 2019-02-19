d41bfe4f45e53fea4bce66b4dd8caea4ef423989
cyuun <41702930+cyuun@users.noreply.github.com>
2019-02-18 23:27:14 -0700

    Add files via upload

---
+++ lab3/part3.ino
@@ -0,0 +1,33 @@
+#include "Volume.h"
+
+//speaker is in pin 5
+const int slider = A0;
+const int spiner = A1;
+
+Volume vol;
+int pitch = 0;
+int volume = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  // vol.begin is necessary for the volume library I'm using
+  vol.begin();
+  // just good ol serial communication
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+  //reading in the analog inputs, I already know theyre potentiometers, so I know the range 
+  volume = map(analogRead(A0), 0, 1023, 0, 255);
+  pitch = map(analogRead(A1), 0, 1023, 120, 3500);
+  
+  //just checking things
+  //Serial.println(vol);
+  //Serial.println(pitch);
+  
+  //outputting with the volume library I'm using, no pin is specified, because the library uses pin 5 by default
+  vol.tone(pitch, volume);
+}
